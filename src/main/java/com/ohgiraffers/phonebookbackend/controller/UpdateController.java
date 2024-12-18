bbae350d5a56abe768f260a3a15a9102ec7c60bf
NJ-KIM11 <alwaysred1116@gmail.com>
2024-12-18 19:20:12 +0900

    sadfas

---
+++ src/main/java/com/ohgiraffers/phonebookbackend/controller/UpdateController.java
@@ -0,0 +1,4 @@
+package com.ohgiraffers.phonebookbackend.controller;
+
+public class UpdateController {
+}
---
+++ src/main/java/com/ohgiraffers/phonebookbackend/service/UpdateService.java
@@ -0,0 +1,4 @@
+package com.ohgiraffers.phonebookbackend.service;
+
+public class UpdateService {
+}
